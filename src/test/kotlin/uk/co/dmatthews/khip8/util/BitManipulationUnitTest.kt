f7f84196b6ed7071be61e786224b5c3c145d9a0a
Danny Matthews <dannymatthews85@gmail.com>
2021-09-24 18:58:16 +0000

    Added tests

--- src/test/kotlin/uk/co/dmatthews/khip8/util/BitManipulationUnitTest.kt
+++ src/test/kotlin/uk/co/dmatthews/khip8/util/BitManipulationUnitTest.kt
@@ -2,12 +2,15 @@ package uk.co.dmatthews.khip8.util
 
 import createBigEndianWordFromBytes
 import leftNibble
+import nibbleByteHex
 import org.junit.jupiter.api.Test
 import rightByte
 import rightNibble
 import rightNibbleByte
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import toHex
+import wordHex
 import x
 import y
 
@@ -66,5 +69,65 @@ class BitManipulationUnitTest {
             createBigEndianWordFromBytes(0x43.toUByte(), 0x21.toUByte())
         ).isEqualTo(0x4321.toUInt()
         )
+    }
+
+    @Test
+    fun `Int to hex converts Int to hex`() {
+        expectThat(toHex(15.toUInt(), 2)).isEqualTo("0x0F")
+    }
+
+    @Test
+    fun `Int to hex with padding of 2`() {
+        expectThat(toHex(0x1.toUInt(), 2)).isEqualTo("0x01")
+    }
+
+    @Test
+    fun `Int to hex with padding of 4`() {
+        expectThat(toHex(0x123.toUInt(), 4)).isEqualTo("0x0123")
+    }
+
+    @Test
+    fun `Int to hex with padding of 4 and value matches limit`() {
+        expectThat(toHex(0x1234.toUInt(), 4)).isEqualTo("0x1234")
+    }
+
+    @Test
+    fun `Int to hex with padding of 4 and value over limit`() {
+        expectThat(toHex(0x12345.toUInt(), 4)).isEqualTo("0x12345")
+    }
+
+    @Test
+    fun `Byte to hex converts Byte to hex`() {
+        expectThat(toHex(15.toUByte())).isEqualTo("0x0F")
+    }
+
+    @Test
+    fun `wordHex converts with padding of 4`() {
+        expectThat(wordHex(0x123.toUInt())).isEqualTo("0x0123")
+    }
+
+    @Test
+    fun `wordHex does not cut off if value is larger`() {
+        expectThat(wordHex(0x12345.toUInt())).isEqualTo("0x12345")
+    }
+
+    @Test
+    fun `wordHex using Int converts with padding of 4`() {
+        expectThat(wordHex(0x123)).isEqualTo("0x0123")
+    }
+
+    @Test
+    fun `wordHex Using Int does not cut off if value is larger`() {
+        expectThat(wordHex(0x12345)).isEqualTo("0x12345")
+    }
+
+    @Test
+    fun `nibbleByteHex converts with padding of 3`() {
+        expectThat(nibbleByteHex(0x12.toUInt())).isEqualTo("0x012")
+    }
+
+    @Test
+    fun `nibbleByteHex does not cut off if value is larger`() {
+        expectThat(nibbleByteHex(0x12345.toUInt())).isEqualTo("0x12345")
     }
 }
