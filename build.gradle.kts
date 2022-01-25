1fbce684fd7d8959559f4e9cd6c8550d0bc3e0f2
Danny Matthews <dannymatthews85@gmail.com>
2022-01-25 22:35:44 +0000

    Amended Gradle build to allow test to run

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,23 +5,10 @@ version = "1.0-SNAPSHOT"
 
 plugins {
     kotlin("multiplatform") version "1.6.10"
-    id("name.remal.sonarlint") version "1.5.0"
-    id("jacoco")
     id("io.kotest.multiplatform") version "5.0.2"
 }
 
 repositories { mavenCentral() }
-
-// Test dependencies
-val mockKVersion = "1.12.1"
-val kotestVersion = "5.0.2"
-val striktVersion = "0.33.0"
-
-// Business logic dependencies
-val hopliteVersion = "1.4.16"
-val kotlinStdLibraryVersion = "1.6.10"
-val koinVersion = "3.1.5"
-val kLoggerVersion = "2.2.0"
 
 kotlin {
     jvm()
@@ -29,44 +16,43 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinStdLibraryVersion")
+                implementation("com.soywiz.korlibs.klogger:klogger:${property("kLogger.version")}")
 
-                implementation("com.soywiz.korlibs.klogger:klogger:$kLoggerVersion")
-
-                implementation("io.insert-koin:koin-core:$koinVersion")
-                implementation("io.insert-koin:koin-ktor:$koinVersion")
+                implementation("io.insert-koin:koin-core:${property("koin.version")}")
+                implementation("io.insert-koin:koin-ktor:${property("koin.version")}")
             }
         }
 
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+                implementation("io.mockk:mockk:${property("mockK.version")}")
+                implementation("io.mockk:mockk-common:${property("mockK.version")}")
 
-//                implementation("io.insert-koin:koin-test:$koinVersion")
-
-                implementation("io.mockk:mockk:$mockKVersion")
-                implementation("io.mockk:mockk-common:$mockKVersion")
-
-                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
-                implementation("io.kotest:kotest-framework-engine:$kotestVersion")
-                implementation("io.kotest:kotest-framework-datatest:$kotestVersion")
+                implementation("io.kotest:kotest-assertions-core:${property("kotest.version")}")
+                implementation("io.kotest:kotest-framework-engine:${property("kotest.version")}")
+                implementation("io.kotest:kotest-framework-datatest:${property("kotest.version")}")
             }
         }
+
         val jvmMain by getting {
             dependencies {
-                implementation("com.sksamuel.hoplite:hoplite-core:$hopliteVersion")
-                implementation("com.sksamuel.hoplite:hoplite-json:$hopliteVersion")
-                implementation("com.soywiz.korlibs.klogger:klogger-jvm:$kLoggerVersion")
+                implementation("com.sksamuel.hoplite:hoplite-core:${property("hoplite.version")}")
+                implementation("com.sksamuel.hoplite:hoplite-json:${property("hoplite.version")}")
+                implementation("com.soywiz.korlibs.klogger:klogger-jvm:${property("kLogger.version")}")
             }
         }
+
         val jvmTest by getting {
             dependencies {
-//                implementation("io.insert-koin:koin-test-junit5:$koinVersion")
+                implementation("io.strikt:strikt-core:${property("strikt.version")}")
 
-                implementation("io.strikt:strikt-core:$striktVersion")
+                implementation("io.kotest:kotest-runner-junit5-jvm:${property("kotest.version")}")
+                implementation("io.kotest:kotest-framework-engine-jvm:${property("kotest.version")}")
 
-                implementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
-                implementation("io.kotest:kotest-framework-engine-jvm:$kotestVersion")
+                implementation("io.insert-koin:koin-test:${property("koin.version")}") {
+                    exclude("org.jetbrains.kotlin", "kotlin-test-junit")
+                }
             }
         }
     }
@@ -83,8 +69,6 @@ tasks {
             freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
         }
     }
-
-    sonarlint { excludes.messages("kotlin:S1135") } // Do not alert for TODOs
 
     withType<Test> {
         useJUnitPlatform()
